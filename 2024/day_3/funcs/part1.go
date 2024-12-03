d3e52dac70712fd0b98432402a3abfd746fb7929
Manuel Köll <man.koell2405@gmail.com>
2024-12-03 20:25:26 +0100

    FEAT: day3 init

---
+++ 2024/day_3/funcs/part1.go
@@ -0,0 +1,15 @@
+package day3
+
+import (
+	"strings"
+	"time"
+	"utilities"
+)
+
+func Parse_answer_one(file_content []byte) float64 {
+	defer utilities.TimeTrack(time.Now(), "Day 3 Part 1")
+	lines := strings.Split(string(file_content), "\n")
+	total := 0
+
+	return float64(total)
+}
---
+++ 2024/day_3/funcs/part2.go
@@ -0,0 +1,14 @@
+package day3
+
+import (
+	"strings"
+	"time"
+	"utilities"
+)
+
+func Parse_answer_two(file_content []byte) float64 {
+	defer utilities.TimeTrack(time.Now(), "Day 3 Part 2")
+	lines := strings.Split(string(file_content), "\n")
+	total := 0
+	return float64(total)
+}
