a73a30800fe0e7dafb9563033be320fe5f212fb9
Manuel Köll <man.koell2405@gmail.com>
2024-12-04 19:21:22 +0100

    feat: starting day 4

--- 2024/init_day.sh
+++ 2024/init_day.sh
@@ -1,9 +1,10 @@
 
 cp -r day_X day_$1
 mv day_$1/dayX day_$1/day$1
-sed -i 's/.\/day_X/.\/day_X\n\t.\/day_'$1'/' go.work
+sed -i 's/.\/day_X/.\/day_'$1'\n\t.\/day_X/' go.work
 sed -i 's/dayX/day'$1'/' day_$1/go.mod
 sed -i 's/dayX/day'$1'/g' day_$1/main.go
+sed -i 's/Day X/Day '$1'/g' day_$1/main.go
 sed -i 's/dayX/day'$1'/g' day_$1/funcs/part1.go
 sed -i 's/dayX/day'$1'/g' day_$1/funcs/part2.go
 sed -i 's/dayX/day'$1'/g' day_$1/test.go
