3c02bae86ceb41c0df8e495af491a797be56fb3e
Manuel Köll <man.koell2405@gmail.com>
2024-12-04 19:18:14 +0100

    feat: starting day 4

---
+++ 2024/day_X/test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	funcs "dayX/funcs"
+	"testing"
+	"utilities"
+)
+
+func TestDay2Part1(t *testing.T) {
+	test_data := utilities.FileReader("data_files/test_part1.txt")
+	expected_answer := 161.0
+	answer := funcs.Parse_answer_one(test_data)
+	if answer != expected_answer {
+		t.Fatalf("Part1 Test does not conform to expected answer: %f / %f\n", answer, expected_answer)
+	}
+}
+
+func TestDay2Part2(t *testing.T) {
+	test_data := utilities.FileReader("data_files/test_part2.txt")
+	expected_answer := 48.0
+	answer := funcs.Parse_answer_two(test_data)
+	if answer != expected_answer {
+		t.Fatalf("Part1 Test does not conform to expected answer: %f / %f\n", answer, expected_answer)
+	}
+}
---
+++ 2024/init_day.sh
@@ -0,0 +1,12 @@
+
+cp -r day_X day_$1
+mv day_$1/dayX day_$1/day$1
+sed -i 's/use (/use (\n\t.\/day_'$1'/' go.work
+sed -i 's/dayX/day'$1'/' day_$1/go.mod
+sed -i 's/dayX/day'$1'/g' day_$1/main.go
+sed -i 's/dayX/day'$1'/g' day_$1/funcs/part1.go
+sed -i 's/dayX/day'$1'/g' day_$1/funcs/part2.go
+sed -i 's/dayX/day'$1'/g' day_$1/test.go
+git add .
+git commit -m "feat: starting day "$1
+cd day_$1
