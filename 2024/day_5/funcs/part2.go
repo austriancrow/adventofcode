e673a8b73dd8760d3f4772bf4535bdf004728a4b
Manuel Köll <man.koell2405@gmail.com>
2024-12-05 19:54:29 +0100

    feat: starting day 5

---
+++ 2024/day_5/funcs/part2.go
@@ -0,0 +1,14 @@
+package day5
+
+import (
+	"time"
+	"utilities"
+)
+
+func Parse_answer_two(file_content []byte) float64 {
+	defer utilities.TimeTrack(time.Now(), "Day 5 Part 2")
+	// lines := strings.Split(string(file_content), "\n")
+	total := 0
+
+	return float64(total)
+}
